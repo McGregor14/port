13a14501d4ea21b2396db2e3b5b6b7d04a47ca9f
Thomas McGregor <thomas.mcgregor@kcl.ac.uk>
2022-05-12 15:34:18 +0100

    added code to save all individual flare datasets in the interim folder

--- r/data_cleaning/flare/01_flare_import.R
+++ r/data_cleaning/flare/01_flare_import.R
@@ -1,23 +1,26 @@
+# Clear the environment
+rm(list = ls())
+
 # Load packages
 library(tidyverse) # data manipulation
 library(here) # file referencing
 library(lubridate) # date/time manipulations
 library(fs) # file system operations
 
-# Source function scripts
-source(file = )
+# Source function script
+source(file = here("r", "functions.R"))
 
 # Specify the subfolder raw data is stored in
-raw_data_loc <- 
+raw_data_loc <- here("data", "raw")
 
 # Find the name of the latest folder exported from the flare website
-flare_data_loc <- find_latest_flare_file(experiment_name = "PORT", path_name = raw_data_loc)
+flare_data_loc <- find_latest_flare_folder(experiment_name = "PORT", path_name = raw_data_loc)
 
 # Create a pathname from the location of the raw data and the latest flare folder export
-flare_data_path <- paste0(raw_data_loc, flare_data_loc)
+flare_data_path <- paste0(raw_data_loc, "/", flare_data_loc)
 
 # List all the csv files stored in the latest flare folders
-flare_csv_files <- fs::dir_ls(flare_data_path, regexp = "\\.csv$")
+flare_csv_files <- dir_ls(flare_data_path, regexp = "\\.csv$")
 
 # Read in all csv files stored in the latest flare folder
 flare_dat_list <- flare_csv_files %>% 
@@ -28,7 +31,7 @@ flare_dat_names <- flare_dat_list %>%
   names() %>% 
   str_remove_all(., paste0(flare_data_path, "/", flare_data_loc, "-")) %>%
   str_remove(., ".csv") %>% 
-  str_replace_all(., "-", "_") 
+  str_replace_all(., "-", "_")
 
 # Set the new names of the list elements
 names(flare_dat_list) <- flare_dat_names
@@ -36,3 +39,11 @@ names(flare_dat_list) <- flare_dat_names
 # Split all elements in the list into separate objects in the environment
 flare_dat_list %>% 
   list2env(., envir = .GlobalEnv)
+
+# Remove original grouped list of datasets from environment
+rm(flare_dat_list)
+
+
+
+# Save all dataframe objects in the global environment
+save_all_dataframes(path = here("data", "interim","flare"))
--- r/functions.R
+++ r/functions.R
@@ -3,19 +3,30 @@
 
 # find_latest_flare_folder:
 # Provides the name of the most recently exported flare folder
-find_latest_flare_file <- function(experiment_name, path_name = "") {
+find_latest_flare_folder <- function(experiment_name, path_name = "") {
   
-  dir_ls(path_name) %>% # Lists the folders in the directory
-    str_replace(., paste0(path_name, experiment_name, "-"), "") %>% # Removes the part of the string before the timestamp
-    as_datetime(.) %>% # Converts the timestamp to a date
+  fs::dir_ls(path_name) %>% # Lists the folders in the directory
+    stringr::str_replace(., paste0(path_name, experiment_name, "-"), "") %>% # Removes the part of the string before the timestamp
+    lubridate::as_datetime(.) %>% # Converts the timestamp to a date
     max(.) %>% # Takes the most recent date from the vector of dates created above
-    format_ISO8601(.) %>% # Converts most recent date back to the ISO 8601 but contains some special characters 
-    str_remove_all(., "-|:") %>% # Removes special characters that appear in the path names
+    lubridate::format_ISO8601(.) %>% # Converts most recent date back to the ISO 8601 but contains some special characters 
+    stringr::str_remove_all(., "-|:") %>% # Removes special characters that appear in the path names
     paste0(experiment_name, "-", ., "Z") # Prepends the experiment name and hyphen and appends a Z to the string
 
   }
 
 
 
-# function 2:
-# etc
+# save_all_objects:
+# Saves all the dataframe objects in the global environment as .Rds files to a specified path
+save_all_dataframes <- function(path){
+  
+  obj_names <- ls(.GlobalEnv)
+  for(i in 1:length(obj_names)) {
+    file <- paste0(path, "/", obj_names[i],".Rds")
+    if(is.data.frame(get(obj_names[i]))){
+      saveRDS(get(obj_names[i], envir = .GlobalEnv), file)
+    }
+  }
+  
+}
