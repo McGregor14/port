51d7570aea00b5aa29efb8509d240d245f6be37e
Thomas McGregor <thomas.mcgregor@kcl.ac.uk>
2022-05-27 11:26:25 +0100

    created script to merge data from all flare modules

--- r/data_cleaning/flare/01-flare-import.R
+++ r/data_cleaning/flare/01-flare-import.R
@@ -23,44 +23,44 @@ flare_data_path <- paste0(raw_data_loc, "/", flare_data_loc)
 flare_csv_files <- dir_ls(flare_data_path, regexp = "\\.csv$")
 
 # Read in all csv files stored in the latest flare folder
-flare_dat_list <- flare_csv_files %>% 
+flare_data_list <- flare_csv_files %>% 
   map(~read_csv(., col_names = TRUE, trim_ws = TRUE))
 
 # Remove the pathname, duplicate prefix, and filetype from the names of the list elements
-flare_dat_names <- flare_dat_list %>%
+flare_data_names <- flare_data_list %>%
   names() %>% 
   str_remove_all(., paste0(flare_data_path, "/", flare_data_loc, "-")) %>%
   str_remove(., ".csv") %>% 
   str_replace_all(., "-", "_")
 
 # Set the new names of the list elements
-names(flare_dat_list) <- flare_dat_names
+names(flare_data_list) <- flare_data_names
 
 # Remove all elements of the list with 0 rows
-flare_dat_list <- keep(flare_dat_list, ~ nrow(.x) > 0)
+flare_data_list <- keep(flare_data_list, ~ nrow(.x) > 0)
 
 # Remove the 'completed participants' dataframe
-flare_dat_list <-  flare_dat_list[names(flare_dat_list) != "completed_participant_ids"]
+flare_data_list <-  flare_data_list[names(flare_data_list) != "completed_participant_ids"]
 
 # Print a message giving the names of modules with no data
-empty_modules <- flare_dat_names[which(flare_dat_names %!in% names(flare_dat_list))]
+empty_modules <- flare_data_names[which(flare_data_names %!in% names(flare_data_list))]
 message(paste0("Modules with no data: ", toString(empty_modules)))
 
 # Remove test participants (without PORT_R_ prefix) and drop ID prefix (PORT_R_)
-flare_dat_list <- map2(flare_dat_list, names(flare_dat_list), ~ filter(.x, str_detect(participant_id, 'PORT_R_')))
+flare_data_list <- map2(flare_data_list, names(flare_data_list), ~ filter(.x, str_detect(participant_id, 'PORT_R_')))
 
-flare_dat_list <- flare_dat_list %>% 
+flare_data_list <- flare_data_list %>% 
   map(modify_at, "participant_id", substring, 8)
 
 # Drop redundant columns shared across all dataframes in the list
-flare_dat_list <- map(flare_dat_list, ~ (.x %>% select(-c('experiment_id', 'experiment_code'))))
+flare_data_list <- map(flare_data_list, ~ (.x %>% select(-c('experiment_id', 'experiment_code'))))
 
 # Split all elements in the list into separate objects in the environment
-flare_dat_list %>% 
+flare_data_list %>% 
   list2env(., envir = .GlobalEnv)
 
 # Remove original grouped list of datasets from environment
-rm(flare_dat_list)
+rm(flare_data_list)
 
 # Save all dataframe objects in the global environment
 save_all_dataframes(path = here("data", "interim","flare", "step-01"))
---
+++ r/data_cleaning/flare/03-flare-merge.R
@@ -0,0 +1,42 @@
+# Clear the environment
+rm(list = ls())
+
+# Load packages
+library(tidyverse) # data manipulation
+library(here) # file referencing
+library(fs) # file system operations
+
+# Specify the location of data files from step 02 of cleaning
+flare_data_path <- (here("data", "interim", "flare", "step-02"))
+
+# List all the RDS files stored in the latest flare folders
+flare_rds_files <- dir_ls(flare_data_path, regexp = "\\.Rds$")
+
+# Read in all RDS files stored in the latest flare folder
+flare_data_list <- flare_rds_files %>% 
+  map(~read_rds(.))
+
+# Remove the pathname, duplicate prefix, and filetype from the names of the list elements
+flare_data_names <- flare_data_list %>%
+  names() %>% 
+  str_remove_all(., paste0(flare_data_path, "/")) %>%
+  str_remove(., ".Rds")
+
+# Set the new names of the list elements
+names(flare_data_list) <- flare_data_names
+
+# Reorder the list ready for merging
+flare_data_list <- flare_data_list[c("participants",
+                                     "basic_info", 
+                                     "affective_ratings", 
+                                     "fear_conditioning", 
+                                     "contingency_awareness", 
+                                     "us_unpleasantness", 
+                                     "post_experiment_questions")]
+
+# Merge all elements of the list
+flare_data <- flare_data_list %>% 
+  reduce(full_join, by = "participant_id")
+
+# Save data
+saveRDS(flare_data, paste0(here("data", "processed"), "/flare_data.Rds"))
