c212dc5d7f920dbded95f88c418545b135176a7a
Thomas McGregor <thomas.mcgregor@kcl.ac.uk>
2022-07-25 10:51:42 +0100

    reverse scored eacs

--- r/data_cleaning/qualtrics/02_qualtrics_clean.R
+++ r/data_cleaning/qualtrics/02_qualtrics_clean.R
@@ -36,9 +36,26 @@ mid_app_data <- mid_app_data %>%
 
 # Reverse score items
 
+# eACS:
+# To use the eACS as a one factor measure, first reverse the following items: 1, 2, 3, 4, 5, 7, 8, 13, 14;
+# then add up the responses for all items to calculate the total score.
+mid_app_data1 <- mid_app_data %>% 
+  mutate(across(.cols = c(eacs_midapp_1,
+                          eacs_midapp_2,
+                          eacs_midapp_3,
+                          eacs_midapp_4,
+                          eacs_midapp_5,
+                          eacs_midapp_7,
+                          eacs_midapp_8,
+                          eacs_midapp_13,
+                          eacs_midapp_14), 
+                ~ 4 + 1 - .)) # Reverse score by taking the maximum value for the scale, adding 1, and subtracting the participant's score.
+
+# ius
+# bis bas
+# epq
 
 # Generate derived variables
-mid_app_data <- mid_app_data %>% 
   
   
 
