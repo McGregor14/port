58445706e08fbc70feeed95a75fda65e81907d59
Thomas McGregor <thomas.mcgregor@kcl.ac.uk>
2022-08-08 17:36:12 +0100

    created phq derived variables

--- r/data_cleaning/redcap/02_redcap_clean_phq.R
+++ r/data_cleaning/redcap/02_redcap_clean_phq.R
@@ -14,49 +14,98 @@ redcap_interim_data_loc <- here("data", "interim", "redcap", "step-01")
 
 # Read in dataset
 phq_data <- read_rds(paste0(redcap_interim_data_loc, "/redcap-phq-data", ".Rds")) %>% 
-  remove_empty(which = c("rows", "cols"))
+  remove_empty(which = c("rows", "cols"))%>% 
+  select(participant_id, matches("\\d$"), everything())
+
+# Rename 
+phq_data_test <- phq_data %>% 
+  # Remove item number from intent question
+  rename_with(
+    .fn = ~str_remove(., "_10"), 
+    .cols = matches('^phq9_intent')
+  )
+
+# Clean existing variables
+phq_data <- phq_data %>% 
+  mutate(
+    
+    # Set all -99 values (prefer not to answer) to NA
+    across(.cols = contains("plans") | contains("preparations") | contains("intent"),
+           ~na_if(., -99)),
+    
+    # Recode 1 to 'yes' and 0 to 'no' for plans and preparations columns
+    across(.cols = contains("plans") | contains("preparations"),
+           ~factor(recode(., 
+                   `1` = "yes",
+                   `0` = "no")))
+  )
 
 # Generate derived variables
 phq_data <- phq_data %>% 
   mutate(
     
-    # phq: Sum of all items
-    phq9_screening_total = rowSums(select(., starts_with("phq9") & contains("screening"))),
-    phq9_baseline_total = rowSums(select(., starts_with("phq9") & contains("baseline"))),
-    phq9_followup_total = rowSums(select(., starts_with("phq9") & contains("followup"))),
+    # phq9: Sum of all individual items (1-9)
+    phq9_screening_total = rowSums(select(., starts_with("phq9") & contains("screening") & matches("\\d$"))),
+    phq9_baseline_total = rowSums(select(., starts_with("phq9") & contains("baseline") & matches("\\d$"))),
+    phq9_followup_total = rowSums(select(., starts_with("phq9") & contains("followup") & matches("\\d$"))),
     
-    # phq7 severity: 0-4 minimal, 5-9 mild, 10-14 moderate, 15+ severe
+    # phq9 severity: 0-4 minimal, 5-9 mild, 10-14 moderate, 15-19 moderately severe, 20+ severe
     phq9_screening_severity = factor(
       case_when(
         phq9_screening_total < 5 ~ "minimal",
         phq9_screening_total < 10 ~ "mild",
         phq9_screening_total < 15 ~ "moderate",
-        phq9_screening_total >= 15 ~ "severe",
+        phq9_screening_total < 20 ~ "moderately_severe",
+        phq9_screening_total >= 20 ~ "severe",
         TRUE ~ NA_character_
       ), 
-      levels = c("minimal", "mild", "moderate", "severe")),
-    
+      levels = c("minimal", "mild", "moderate", "moderately_severe", "severe")),
+
     phq9_baseline_severity = factor(
       case_when(
         phq9_baseline_total < 5 ~ "minimal",
         phq9_baseline_total < 10 ~ "mild",
         phq9_baseline_total < 15 ~ "moderate",
-        phq9_baseline_total >= 15 ~ "severe",
+        phq9_baseline_total < 20 ~ "moderately_severe",
+        phq9_baseline_total >= 20 ~ "severe",
         TRUE ~ NA_character_
       ), 
-      levels = c("minimal", "mild", "moderate", "severe")),
+      levels = c("minimal", "mild", "moderate", "moderately_severe", "severe")),
     
     phq9_followup_severity = factor(
       case_when(
         phq9_followup_total < 5 ~ "minimal",
         phq9_followup_total < 10 ~ "mild",
         phq9_followup_total < 15 ~ "moderate",
-        phq9_followup_total >= 15 ~ "severe",
+        phq9_followup_total < 20 ~ "moderately_severe",
+        phq9_followup_total >= 20 ~ "severe",
         TRUE ~ NA_character_
       ), 
-      levels = c("minimal", "mild", "moderate", "severe"))
+      levels = c("minimal", "mild", "moderate", "moderately_severe", "severe")),
     
-  )
+    # phq9 binary depression: >=10 - depression diagnosis
+    phq9_screening_binary_depression = 
+      case_when(
+        phq9_screening_total < 10 ~ FALSE,
+        phq9_screening_total >= 10 ~ TRUE,
+        TRUE ~ NA
+      ),
+    
+    phq9_baseline_binary_depression = 
+      case_when(
+        phq9_baseline_total < 10 ~ FALSE,
+        phq9_baseline_total >= 10 ~ TRUE,
+        TRUE ~ NA
+      ),
+    
+    phq9_followup_binary_depression = 
+      case_when(
+        phq9_followup_total < 10 ~ FALSE,
+        phq9_followup_total >= 10 ~ TRUE,
+        TRUE ~ NA
+      )
+   
+  ) 
 
 # Save data
 saveRDS(phq_data, here("data", "interim", "redcap", "step-02", "redcap-phq-data.Rds"))
